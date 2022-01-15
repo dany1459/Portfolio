fe1909526d72093d5ae94440bcf1b8f3630f9c42
Dan Adrian <dany1459@yahoo.co.uk>
2022-01-15 08:19:25 +0100

    Create projects.py

---
+++ Portfolio/website/static/projects.py
@@ -0,0 +1,40 @@
+
+projects = [
+    {
+        'image': 'static/best_books.png',
+        'title': 'Best Books',
+        'link': 'https://github.com/dany1459/AI-Engineering/tree/main/Chapter%201/Best%20Books%20Ever%20Challenge',
+        'description': 'An in-depth analysis of the top 5000 books of all time.'
+    },
+    {
+        'image': 'static/trackit_fitness.png',
+        'title': 'Trackit Fitness',
+        'link': 'https://github.com/dany1459/AI-Engineering/tree/main/Chapter%202/Trackit%20Fitness%20App',
+        'description': """Trackit is an android app that allows the users to keep track of their daily movements. 
+It communicates with the sensors in the phone to collect data describing the users' engagements in the day. 
+Users have the option of turning on these sensors by just clicking a button on the app. 
+Once the users turns it off, they can choose to save this data to disk. 
+Then the users can click a button to see the analysis of their day's movements. 
+The app summarizes the movement means in actions done by standing still, walking and being in a mode of transportation.
+It gives the total distances grouped by movement, total time, and estimated calories burned. 
+It goes on to recommend suitable activities the users can engage in the following day."""
+    },
+    {
+        'image': 'static/s.png',
+        'title': 'Kaggle Fashion Challenge',
+        'link': 'https://github.com/dany1459/AI-Engineering/tree/main/Chapter%203/04.%20MLP%20Kaggle%20challenge',
+        'description': 'DESCR'
+    },
+    {
+        'image': 'static/b.png',
+        'title': 'Fitness Pygame',
+        'link': 'https://github.com/dany1459/AI-Engineering/tree/main/Chapter%201/Best%20Books%20Ever%20Challenge',
+        'description': 'DESCR'
+    },
+    {
+        'image': 'static/b.png',
+        'title': 'Self-driving Cars',
+        'link': 'https://github.com/dany1459/AI-Engineering/tree/main/Chapter%203/09.%20Computer%20Vision%20Team%20Project',
+        'description': 'DESCR'
+    }
+]
