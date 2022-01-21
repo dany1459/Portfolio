c05ae6850a3853f1b655d5694f6a496e6151f51b
Dan Adrian <dany1459@yahoo.co.uk>
2022-01-21 10:24:19 +0100

    D

--- Portfolio/website/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-from sqlalchemy.sql import func
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), unique=False)
-    email = db.Column(db.String(150), unique=False)
-    message = db.Column(db.String(5000), unique=False)
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
