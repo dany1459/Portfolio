efa9f37cc0ec400eb33ebeb3eac928c64a24c84c
Dan Adrian <dany1459@yahoo.co.uk>
2022-02-05 12:09:36 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -24,3 +24,14 @@ def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('database created!')
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+#     api = Api(app)
+#     db.init_app(app) # initialize the database
+#     db.create_all(app=app) # create tables
+#     api.add_resource(HomeRoute, '/')
+#     api.add_resource(HomeRouteWithId, '/<string:id>')
+#     return app
