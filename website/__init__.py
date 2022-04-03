369d71465aecdb33902b8cbf276dbb7ef4fd7697
Dan Adrian <dany1459@yahoo.co.uk>
2022-04-03 12:34:11 +0200

    update

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from os import path
 
 db = SQLAlchemy()
@@ -7,31 +8,28 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'qwerty'
+    app.config['SECRET_KEY'] = 'n7v59345v94i73m7v54z37v54358304v594309vn5n4307n50437v50v3409543v7543v075'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
+    from .auth import auth
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-    from .models import Message
+    from .models import User, Note, Message
     create_database(app)
 
-    return app
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
-        print('database created!')
-
-# def create_app():
-#     app = Flask(__name__)
-#     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-#     api = Api(app)
-#     db.init_app(app) # initialize the database
-#     db.create_all(app=app) # create tables
-#     api.add_resource(HomeRoute, '/')
-#     api.add_resource(HomeRouteWithId, '/<string:id>')
-#     return app
